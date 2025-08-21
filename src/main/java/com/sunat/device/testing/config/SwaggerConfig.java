009327103eb004dd4ddd1fd1af9adb31ccc62fbc
Jhonatan-Polar <jhonatan.cueva@pucp.edu.pe>
2025-08-20 20:30:58 -0500

    Define Header and Detail for Docs

---
+++ src/main/java/com/sunat/device/testing/config/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package com.sunat.device.testing.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("User CRUD API")
+                        .description("Ejemplo de API con Spring Boot, H2 y Swagger/OpenAPI")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("Equipo Dev")
+                                .url("https://miempresa.com")
+                                .email("dev@miempresa.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0")))
+                .servers(List.of(
+                        new Server().url("http://localhost:8080").description("Servidor local"),
+                        new Server().url("https://api.miempresa.com").description("Servidor Producción")
+                ))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Documentación completa")
+                        .url("https://miempresa.com/docs"));
+    }
+}
