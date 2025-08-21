94ed3fa36a3e6b228deef552e70fd2d64576ce3f
Jhonatan-Polar <jhonatan.cueva@pucp.edu.pe>
2025-08-20 20:42:16 -0500

    Define Schema with example

--- src/main/java/com/sunat/device/testing/entity/User.java
+++ src/main/java/com/sunat/device/testing/entity/User.java
@@ -1,5 +1,6 @@
 package com.sunat.device.testing.entity;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,11 +9,16 @@ import lombok.Setter;
 @Getter
 @Setter
 @Table(name = "users")
+@Schema(description = "Entidad Usuario")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(description = "Identificador único", example = "1")
     private Long id;
 
+    @Schema(description = "Nombre completo", example = "Juan Pérez")
     private String name;
+
+    @Schema(description = "Correo electrónico", example = "juan@correo.com")
     private String email;
 }
